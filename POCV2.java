eeb7deb0cbd96c76ee146fa32bd37e49ee2db696
thomasmcinnes <43774905+thomasmcinnes@users.noreply.github.com>
2018-11-12 14:55:22 +0000

    POC 0.3.0
    I have added in method to:
    multiplying the estimated number by how many voted it
    The duplicate values are the power of the amount people voted it. 
    All Warning removed and code smells.

--- POCV2.java
+++ POCV2.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
 
-public class POCV2<groupScore, rowResult> {
+public class POCV2 {
     public static void main(String[] args)throws Exception {
         String weekCSV = "C:\\Agile Poker\\scores.csv";
         //location of file \\ are escape characters. So code can compile the code.
@@ -23,24 +21,41 @@ public class POCV2<groupScore, rowResult> {
         int groupScore = 0;
         int rowResult = 0;
 
-
         for (String lineInFile : voterResults) {
             // lineInFile is the row containing all the estimates
             //voterResults is the name for each row
 
-            int[] numbers = Arrays.stream(lineInFile.split(",")).mapToInt(Integer::parseInt).toArray();
+            int[] numbers = Arrays.stream(lineInFile.split(",")).mapToInt(Integer::valueOf).toArray();
             //Array numbers is name. Estimates split when there a comma
             groupScore = groupScore + numbers[numbers.length-1];
             //Currently printing off the last result of each row and adding them up.
             rowResult = numbers[numbers.length-1];
 
+            Map<Integer, Integer> valueCount = new HashMap<>();
+            for(Integer number : numbers) {
+                //looking at each numbers.
+                valueCount.put(number, valueCount.getOrDefault(number, 0) + 1);
+                // look at the values in the numbers. what are same and then what duplicates and add by one.
+                // it does it for each row.
+//                forMatch = (number * numbers);
+            }
+            for(Map.Entry<Integer, Integer> mapEntry : valueCount.entrySet()) {
+                System.out.println(mapEntry.getKey() + "x" + mapEntry.getValue() + "=" + mapEntry.getKey() * mapEntry.getValue());
+                // this is muliplying the estimate number by how many voted it. Could be valuable later on.
+                System.out.println("The duplicate values are power of the amount people voted it" + Math.pow(mapEntry.getKey(),mapEntry.getValue()));
+            }
+
+            System.out.println("Duplicate values are: " + valueCount);
+            //System.out.println(forMatch);
+            //prints each rows duplicates.
         }
-        String firstNum = "1";
+
+
         System.out.println("this is all the estimate results added together : "+ groupScore);
         //the results added up
-        System.out.println(rowResult);
-        //print last estimate on a row 
-
+        System.out.println("This is the last number(group estimate) : " + rowResult);
+        //print last estimate on a row
+        //System.out.println(withoutDuplicates);
 
     }
 }
